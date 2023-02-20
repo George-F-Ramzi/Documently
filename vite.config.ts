2488078f41d286c3e1a26b4e1712f53646d74724
George-F-Ramzi <109841377+George-F-Ramzi@users.noreply.github.com>
2023-02-20 17:50:33 +0200

    publsing

--- vite.config.ts
+++ vite.config.ts
@@ -4,4 +4,11 @@ import react from "@vitejs/plugin-react-swc";
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  build: {
+    rollupOptions: {
+      moduleContext: {
+        "./node_modules/pdfmake/build/vfs_fonts.js": "window",
+      },
+    },
+  },
 });
